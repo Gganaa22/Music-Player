322f847f30232ca58680292c9c365c750888e09a
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-06-12 17:05:00 +0800

    MySQL deh ogogdliin santaigaa holbogdohiin tuld DBConnection class uusgev

---
+++ music_player_system/src/main/java/com/music_player/Database/DBConnection.java
@@ -0,0 +1,20 @@
+package com.music_player.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection{
+
+    private static final String URL = "jdbc:mysql://localhost:3306/gbeat_db";
+    private static final String USER = "root";
+    private static final String PASSWORD ="Admin123.";
+
+    public static Connection getConnection(){
+        try{
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch(Exception e){
+            System.out.println("DB Connection Error:"+e.getMessage());
+            return null;
+        }
+    }
+}
