0ed44a5c0b16edf2515b792e7865826371f73125
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-06-12 16:54:36 +0800

    module-info.java bolon pom iin dependencies hesegt MySQL iin tohirgoonuudiig hiij nemev

--- music_player_system/src/main/java/module-info.java
+++ music_player_system/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.music_player {
     requires javafx.controls;
     requires javafx.fxml;
+    //nemsen
+    requires java.sql;
 
     opens com.music_player to javafx.fxml;
     exports com.music_player;
