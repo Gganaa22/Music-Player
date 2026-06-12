4178db5feecd27487b3b70b2330729fa7073d682
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-06-12 17:08:06 +0800

    Song class uusgev

---
+++ music_player_system/src/main/java/com/music_player/Database/Song.java
@@ -0,0 +1,10 @@
+package com.music_player.Database;
+
+public class Song{
+    private int id;
+    private String title;
+    private String artist;
+    private String genre;
+    private String fileName;
+    private int playCount;
+}
