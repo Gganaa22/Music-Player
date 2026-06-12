341eb11095e37f77757fe330dc5c2e189ef0d0fd
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-06-12 20:11:46 +0800

    Song class deeree uildel nemj bicij ogsn bas SongDAO class uusgesen

---
+++ music_player_system/src/main/java/com/music_player/Database/SongDAO.java
@@ -0,0 +1,39 @@
+package com.music_player.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import com.music_player.Model.Song;
+
+public class SongDAO {
+
+    public static List<Song> getAllSongs(){
+        List<Song> list = new ArrayList<>();
+
+        try {
+            Connection conn = DBConnection.getConnection();
+            String sql = "SELECT * FROM songs";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Song s = new Song();
+                s.setId(rs.getInt("id"));
+                s.setTitle(rs.getString("title"));
+                s.setArtist(rs.getString("artist"));
+                s.setGenre(rs.getString("genre"));
+                s.setFileName(rs.getString("file_name"));
+                s.setPlayCount(rs.getInt("play_count"));
+
+                list.add(s);
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return list;
+    }
+
+}
---
+++ music_player_system/src/main/java/com/music_player/Model/Song.java
@@ -0,0 +1,60 @@
+package com.music_player.Model;
+
+public class Song{
+    private int id;
+    private String title;
+    private String artist;
+    private String genre;
+    private String fileName;
+    private int playCount;
+
+    public Song() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(int playCount) {
+        this.playCount = playCount;
+    }
+}
