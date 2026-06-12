70a313c6c67ec85a34e6914aa94c4f1aa10ea7d7
Gganaa22 <darkhanbaatargantogtokh47@gmail.com>
2026-06-12 21:10:38 +0800

    tableView deeree MySQL deer uusgesen ogogdluudee gargaj irev

--- music_player_system/src/main/java/com/music_player/Controller/MainController.java
+++ music_player_system/src/main/java/com/music_player/Controller/MainController.java
@@ -1,10 +1,16 @@
 package com.music_player.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableColumn;
+import com.music_player.Database.SongDAO;
+import com.music_player.Model.Song;
 
 public class MainController {
 
@@ -12,12 +18,37 @@ public class MainController {
     private Button btnAddSong;
 
     @FXML
+    private TableColumn<Song, String> colArtist;
+
+    @FXML
+    private TableColumn<Song, String> colGenre;
+
+    @FXML
+    private TableColumn<Song, String> colTitle;
+
+    @FXML
     private ListView<?> listPlayeList;
 
     @FXML
-    private TableView<?> tblSongs;
+    private TableView<Song> tblSongs;
 
     @FXML
     private TextField txtSearch;
 
+    @FXML
+    public void initialize() {
+
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colArtist.setCellValueFactory(new PropertyValueFactory<>("artist"));
+        colGenre.setCellValueFactory(new PropertyValueFactory<>("genre"));
+
+        ObservableList<Song> songs = FXCollections.observableArrayList(SongDAO.getAllSongs());
+
+        tblSongs.setItems(songs);
+    
+        
+    }
+
 }
+
+
--- music_player_system/src/main/java/module-info.java
+++ music_player_system/src/main/java/module-info.java
@@ -5,5 +5,10 @@ module com.music_player {
     requires java.sql;
 
     opens com.music_player to javafx.fxml;
+    opens com.music_player.Controller to javafx.fxml;
+    opens com.music_player.Model to javafx.fxml;
+
+    exports com.music_player.Model;
     exports com.music_player;
+    exports com.music_player.Controller;
 }
